a3a0c5de5ebc4c00c8f1fd9bb155f6ef92f715e2
Austin Harrison <austin.harrisonn@yahoo.com>
2024-10-01 14:59:17 -0400

    Initial Commit

---
+++ BeReal-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeReal-Clone
+//
+//  Created by Austin Harrison on 10/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
